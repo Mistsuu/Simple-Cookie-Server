4af1700353844216aa50097759c3f907bbd723a1
m3ltus <Mistsuu>
2020-06-05 15:40:51 +0700

    Typical send-response protocol

--- cookie-server.c
+++ cookie-server.c
@@ -4,13 +4,9 @@
 #include <string.h>       // memset()
 #include <arpa/inet.h>    // close()
 #include <unistd.h>       // close()
-#include <pthread.h>      // pthread_create(), pthread_join()
-#include <errno.h>
 #include <sys/time.h>     // FD_SET(), FD_ISSET(), FD_ZERO(), FD_CLR()
 #include <stdlib.h>       // atoi()
 #include <sys/select.h>   // select()
-#include <termios.h>      // ---()
-#include <fcntl.h>        // fcntl()
 #include "map.h"
 
 #define PORT 8784
@@ -18,16 +14,17 @@
 #define BUFSIZE 1024
 
 /* Socket data */
-int port;
 int listenfd;
 int serverfds[MAXCONN];
 char buf[BUFSIZE];
+socklen_t slen = sizeof(struct sockaddr_in);
+struct sockaddr_in saddr;
 /* Cookie map */
 map cookie_map;
 
-char http_request_header[] = {
-    "HTTP/2.0 200 OK\n\rContent-Type: text/html\n\r"
-};
+char http_request_header    [] = "HTTP/2.0 200 OK\n\rContent-Type: text/html\n\r";
+char invalid_request_message[] = "Invalid Request!\n";
+char good_job_message       [] = "Good job babe!\n";
 
 int setup() {
     ///////////////////////////////// COOKIE ///////////////////////////////////
@@ -41,7 +38,7 @@ int setup() {
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family      = AF_INET;
     saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    saddr.sin_port        = htons(port);
+    saddr.sin_port        = htons(PORT);
 
     // Enable address reuse
     int opt = 1;
@@ -69,70 +66,49 @@ int setup() {
     return 0;
 }
 
-void close_connection(int i, int forced=0) {
-    // If not forced, close willingly
-    if (!forced) {
-        shutdown(serverfds[i], SHUT_RDWR);
-        char c; while(read(serverfds[i], &c, 1) >= 0);
-    }
-
+void close_connection(int server_no) {
     // Close socket
-    close(serverfds[i]);
-    serverfds[i] = 0;
+    close(serverfds[server_no]);
+    serverfds[server_no] = 0;
 }
 
-int header_match(char* buf) {
-    int p = 0;
-    for (; p < strlen(http_request_header); ++p)
-        if (http_request_header[p] != buf[p])
-            return -1;
-    return p;
-}
+#define response(server_no, response_message) write(serverfds[server_no], response_message, sizeof(response_message))
 
-void handle_request(int server_no, char* buf, int beg) {
-    if
+void handle_request(int server_no, char* buf) {
+    // char *header, *key, *value;
+    // if ((header = strtok(buf, http_request_header)) == NULL) {
+    //     write(serverfds[server_no], invalid_request_message, sizeof(invalid_request_message));
+    //     return;
+    // }
+    response(server_no, good_job_message);
 }
 
 void receive_from_server(int server_no) {
-    // Abruptedly exit
-    if (read(serverfds[server_no], buf, BUFSIZE) <= 0) {
-        printf("[!] Server abruptedly exit. Doesn't know why...\n");
-        close_connection(server_no, 1);
-        return;
-    }
-
-    // Check string header...
-    int p;
-    if ((p = header_match(buf)) == -1) {
-        printf("[!] Invalid request header!]\n");
-        close_connection(server_no);
-    }
-
+    // Can't receive
+    if (read(serverfds[server_no], buf, BUFSIZE) <= 0) printf("[!] Server abruptedly exit. Doesn't know why...\n");
     // Handle request from server
-    handle_request(server_no, buf, p);
-
-    // Properly exit
+    else                                               handle_request(server_no, buf);
+    // Exit
     close_connection(server_no);
 }
 
 void handle_new_connection() {
-    socklen_t slen = sizeof(struct sockaddr_in);
-    struct sockaddr_in saddr;
-
     int newsockfd = accept(listenfd, (struct sockaddr*) &saddr, &slen);
     if (newsockfd < 0)
         printf("[!] Error accepting socket!\n");
     else for (int i = 0; i < MAXCONN; ++i)
-        if (serverfds[i] == 0)
+        if (serverfds[i] == 0) {
             serverfds[i] = newsockfd;
+            return;
+        }
 }
 
 void run_cookie_server() {
     fd_set readfds;
     int    maxfd;
 
-    while (true) {
-        // Reset & set the socket set
+    while (1) {
+        ///// Reset & set the socket set
         FD_ZERO(&readfds);
         maxfd = listenfd;
         FD_SET(listenfd, &readfds);
@@ -143,14 +119,14 @@ void run_cookie_server() {
                 FD_SET(serverfds[i], &readfds);
             }
 
-        // Wait for activity
+        ///// Wait for activity
         select(maxfd + 1, &readfds, NULL, NULL, NULL);
 
-        // New connection...
+        ///// New connection...
         if (FD_ISSET(listenfd, &readfds))
             handle_new_connection();
 
-        // Server says something....
+        ///// Server says something....
         for (int i = 0; i < MAXCONN; ++i)
             if (FD_ISSET(serverfds[i], &readfds))
                 receive_from_server(i);
